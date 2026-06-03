f3cac0e48678206eb6e013343ece720c51a5f646
kaduox <happy@kaduoxMacBook-Air.local>
2026-06-03 23:18:26 +0800

    feat(core): initialize maven spring boot project

---
+++ src/main/java/com/semirisk/SemiRiskApplication.java
@@ -0,0 +1,13 @@
+package com.semirisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiRiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semirisk/SemiRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semirisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
